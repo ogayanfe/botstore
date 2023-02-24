159e7c658b203642639d749038a6ec715051d627
Odule Ayanfeoluwa <ayanfeoluwakoyinsola@gmail.com>
2023-02-24 18:00:22 +0100

    Used a function in a new utils.tsx file to get data grid column defination

--- frontend/src/pages/dashboard/storedetails/products.tsx
+++ frontend/src/pages/dashboard/storedetails/products.tsx
@@ -17,101 +17,7 @@ import ProductCreateUpdateModal from "../../../components/AddProductModal";
 import { GridRenderCellParams } from "@mui/x-data-grid/models";
 import styled from "@emotion/styled";
 import { useThemeContext } from "../../../context/themeContext";
-
-const columns: GridColDef[] = [
-    {
-        field: "id",
-        headerName: "Product Id",
-        flex: 1,
-        minWidth: 100,
-    },
-    { field: "name", headerName: "Name", flex: 1, minWidth: 100 },
-    {
-        field: "thumbnail",
-        headerName: "Image Thumbnail",
-        sortable: false,
-        filterable: false,
-        editable: false,
-        minWidth: 100,
-        flex: 1,
-        disableColumnMenu: true,
-        align: "center",
-        renderCell({ row }: GridRenderCellParams) {
-            return (
-                <Avatar
-                    variant="rounded"
-                    src={row.thumbnail}
-                    alt={row.name + " product thumbnail"}
-                    sx={{ width: "40px", height: "40px" }}
-                />
-            );
-        },
-    },
-    { field: "category", headerName: "Category", flex: 1, minWidth: 100 },
-    { field: "price", headerName: "Price", flex: 1 },
-    { field: "stock_amount", headerName: "Amount In Stock", flex: 1, minWidth: 100 },
-    { field: "weight", headerName: "Weight", flex: 1, minWidth: 100 },
-    {
-        field: "is_public",
-        minWidth: 100,
-        flex: 1,
-        headerName: "Public",
-        renderCell: ({ row }: GridRenderCellParams) => {
-            const isPubStr = row.is_public.toString();
-            return (
-                <Chip
-                    label={isPubStr[0].toUpperCase() + isPubStr.slice(1)}
-                    size="small"
-                    variant="outlined"
-                    color={row.is_public ? "success" : "secondary"}
-                ></Chip>
-            );
-        },
-    },
-    {
-        field: "update",
-        headerName: "Update",
-        sortable: false,
-        filterable: false,
-        flex: 1,
-        editable: false,
-        minWidth: 100,
-        disableColumnMenu: true,
-        renderCell() {
-            return (
-                <Chip
-                    label="update"
-                    size="small"
-                    color="info"
-                    onClick={console.log}
-                    variant="outlined"
-                />
-            );
-        },
-    },
-    {
-        field: "delete",
-        headerName: "Delete",
-        sortable: false,
-        filterable: false,
-        editable: false,
-        flex: 1,
-        minWidth: 100,
-        disableColumnMenu: true,
-        renderCell() {
-            return (
-                <Chip
-                    label="delete"
-                    size="small"
-                    color="error"
-                    variant="outlined"
-                    icon={<ClearIcon />}
-                    onClick={console.log}
-                />
-            );
-        },
-    },
-];
+import { getGridCols } from "./utils";
 
 interface ProductType {
     id: number;
@@ -160,6 +66,7 @@ function getDataGrid(darkTheme: boolean, width: number) {
         };
     });
 }
+
 function ProductList() {
     const { data: productsList } = useLoaderData() as { data: ProductType[] };
     const { darkTheme } = useThemeContext();
@@ -182,6 +89,7 @@ function ProductList() {
         };
     }, []);
 
+    const columns = getGridCols(console.log);
     return (
         <div className="px-2 h-full md:px-4 lg:px-8 pb-10 w-full max-w-[d1018px]">
             <StickyDataGrid
